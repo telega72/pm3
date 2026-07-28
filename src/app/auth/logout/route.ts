9295c343e69077d9a2218703a1de35a5a4e47be2
telega72 <telega72@gmail.com>
2026-07-28 13:53:14 +0300

    Delete src directory

--- src/app/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { signOut } from "@/lib/auth";
-
-export async function GET(request: Request) {
-  await signOut();
-  return NextResponse.redirect(new URL("/", request.url));
-}
